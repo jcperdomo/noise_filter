e71b508fddc4215b9b93f1e0112e1832874b2a5d
juan carlos perdomo <jcperdomosilva@gmail.com>
2017-03-27 20:48:25 -0400

    Working on the front end

--- noise_filter/views.py
+++ noise_filter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Welcome to the Home Page")
+    return render(request, 'noise_filter/index.html', {})
