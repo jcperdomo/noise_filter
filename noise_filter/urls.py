3b4b2f0b4f0e0ff6322b05f6f209fb178b3db247
Samuel Cheng <samuelcheng@college.harvard.edu>
2017-03-28 21:00:32 -0400

    started the predict api path

--- noise_filter/urls.py
+++ noise_filter/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<image_id>[0-9]+)/$', views.image, name='image'),
+    url(r'^noise_filter/getRandomImage$', views.rand_image, name='rand_image'),
+    url(r'^predict/(?P<image_id>[0-9]+)/', views.predict, name='predict')
 ]
--- noise_filter/views.py
+++ noise_filter/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.dates import DateFormatter
 from .models import Images
 
+from IPython import embed
+
+from os.path import join
+import json
+
+# TODO this will uncompress mnist every time a request comes in, very messy
+from .nnetmnist import NNetMnist
+
+
+# checkpoint_path = join("..", "checkpoint")
+# embed()
+checkpoint_path = "checkpoint"
+nn = NNetMnist(dirname=checkpoint_path)
+nn.fit(epochs=40, skip_if_trained=True, verbose=True)
+
 def index(request):
     return render(request, 'noise_filter/index.html', {})
 
-def image(request, image_id):
-    print "Image, ", image_id
+def rand_image(request):
+    # TODO generate random image id
+    image_id = 7
     im = Images.objects.filter(id=image_id)[0].get_array().reshape(28, 28)
     fig = Figure()
     ax = fig.add_subplot(111)
@@ -18,3 +35,12 @@ def image(request, image_id):
     response = HttpResponse(content_type='image/png')
     canvas.print_png(response)
     return response
+
+@csrf_exempt
+def predict(request, image_id):
+    if request.method == "GET":
+        im = Images.objects.filter(id=image_id)[0].get_array()
+        # embed()
+        return HttpResponse("test")
+    else:
+        return HttpResponse("Use GET to send data")
